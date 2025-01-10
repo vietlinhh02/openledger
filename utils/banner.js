054e3ccaf81f1e8d4f71b17c01ad495a11e5e072
vietlinhh02 <nvlinh0607@gmail.com>
2025-01-11 01:07:16 +0700

    first commit

---
+++ utils/banner.js
@@ -0,0 +1,5 @@
+const banner = `
+
+                        Running Open-Ledger CLI Version                
+                 t.me/zlkcybergroup *** github.com/vietlinhh02                  \n`
+export default banner;
